cf097a1dac623e3c2bd4f6f3dd6c9c31625483ff
Oskar Stenberg <01ste02@gmail.com>
2022-04-08 21:30:42 +0200

    Switch to python implementation

--- icecast-plugin/run.sh
+++ icecast-plugin/run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 echo "Starting softwares...."
-librespot jq -v -n "Hela huset" --passthrough --backend pipe --device /spotify_stream &
-ices /ices.xml < /spotify_stream &
-icecast -c /icecast.xml
+#librespot jq -v -n "Hela huset" --passthrough --backend pipe --device /spotify_stream &
+#ices /ices.xml < /spotify_stream &
+#icecast -c /icecast.xml
+
+python3 librespot_handler.py
